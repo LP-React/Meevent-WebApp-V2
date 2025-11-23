b34f3c57091cceba4c8257edcc89c1cfa6da8e4f
LP-React <ljamirp30@gmail.com>
2025-11-23 12:38:53 -0500

    feature/next-intl

---
+++ i18n/request.ts
@@ -0,0 +1,18 @@
+import { getRequestConfig } from 'next-intl/server';
+import { hasLocale } from 'next-intl';
+import { routing } from './routing';
+
+export default getRequestConfig(async ({ requestLocale }) => {
+
+  let locale = await requestLocale;
+
+  // Ensure that a valid locale is used
+  if (!locale || !routing.locales.includes(locale as any)) {
+    locale = routing.defaultLocale;
+  }
+
+  return {
+    locale,
+    messages: (await import(`../messages/${locale}.json`)).default
+  };
+});
