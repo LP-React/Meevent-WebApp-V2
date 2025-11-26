5679df435a793c6ba1164af43e23c18b63d14e18
OrtegaDev <aaron5ortega123@gmail.com>
2025-11-25 23:33:06 -0500

    se aplico el tema en el nav y footer

--- components/Nabvar.tsx
+++ components/Nabvar.tsx
@@ -2,7 +2,7 @@
 
 import { useTranslations } from "next-intl"
 import Link from "next/link"
-
+import { ArrowRightToLine, LogOut, Moon, Sun } from 'lucide-react';
 
 export const Nabvar = () => {
 
@@ -16,39 +16,42 @@ export const Nabvar = () => {
             </Link>
 
             <div className="w-[900px] flex justify-evenly">
-                <Link href="#" className="no-underline text-black transition duration-200 hover:text-red-500">{t("find_event")}</Link>
-                <Link href="#" className="no-underline text-black transition duration-200 hover:text-red-500">{t("create_event")}</Link>
-                <Link href="#" className="no-underline text-black transition duration-200 hover:text-red-500">{t("find_plans")}</Link>
-                <Link href="#" className="no-underline text-black transition duration-200 hover:text-red-500">{t("create_plan")}</Link>
+                <Link href="#" className="no-underline text-foreground transition duration-200 hover:text-red-500">{t("find_event")}</Link>
+                <Link href="#" className="no-underline text-foregroun transition duration-200 hover:text-red-500">{t("create_event")}</Link>
+                <Link href="#" className="no-underline text-foregroun transition duration-200 hover:text-red-500">{t("find_plans")}</Link>
+                <Link href="#" className="no-underline text-foregroun transition duration-200 hover:text-red-500">{t("create_plan")}</Link>
             </div>
 
 
             <div className="h-full w-[360px] flex justify-evenly items-center">
 
                 <div className="flex items-center justify-evenly w-[70px]">
-                    <div className="cursor-pointer transition duration-200 hover:text-red-500">EN</div>
+                    <div className="cursor-pointer transition duration-200 hover:text-red-500 mr-1">EN</div>
                     <div className="cursor-pointer transition duration-200 hover:text-red-500">ES</div>
                 </div>
 
-                <button className="border-2 px-2 rounded-lg">Oscuro</button>
-                <button className="border-2 px-2 rounded-lg">Claro</button>
+                <Moon className="cursor-pointer duration-200 mx-3 hover:text-red-500"/>
+                <Sun className="cursor-pointer duration-200 mx-3 hover:text-red-500"/>
+                {/* <button className="border-2 px-2 rounded-lg">Oscuro</button>
+                <button className="border-2 px-2 rounded-lg">Claro</button> */}
 
 
                 {/* Sin session */}
                 <div className="flex justify-evenly w-[220px]">
-                    <Link href="#" className="no-underline text-black transition duration-200 hover:text-red-500">{t('sign_up')}</Link>
-                    <Link href="#" className="no-underline text-black transition duration-200 hover:text-red-500">{t('login')}</Link>
+                    <Link href="#" className="no-underline text-foregroun transition duration-200 hover:text-red-500">{t('sign_up')}</Link>
+                    <Link href="#" className="no-underline text-foregroun transition duration-200 hover:text-red-500">{t('login')}</Link>
                 </div>
 
 
                 {/* Con session */}
                 <div className="flex justify-center items-center h-full cursor-pointer transition duration-200 hover:text-red-500">
                     <img src="#" alt="" className="h-[75%] shadow-sm" />
-                    <div className="ml-2.5">Nombre-X</div>
+                    <div className="ml-2.5">name</div>
                 </div>
 
 
                 <div className="cursor-pointer transition duration-200 hover:text-red-500">
+                    <LogOut/>
                     {/* icono de cerrar session */}
                 </div>
 
