a1aa999c6725895501a3fde9aee185768c1decdb
Laysson Polo <109695735+LP-React@users.noreply.github.com>
2025-12-06 11:43:16 -0500

    Feature/auth.js-navbar (#17)
    * feat/config auth.js
    
    * feature/login-navbar
    
    * fix/UserMenu-Image

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,16 +1,8 @@
-import { useTranslations } from "next-intl";
-
 export default function Home() {
-
-  const t = useTranslations('Navbar')
-
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans  bg-background text-foreground bg-">
+    <div className="flex min-h-screen items-center justify-center font-sans  bg-background text-foreground">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight">
-            {t('title')}
-          </h1>
         </div>
       </main>
     </div>
--- components/UserMenu.tsx
+++ components/UserMenu.tsx
@@ -16,8 +16,10 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { Link, usePathname } from "@/i18n/navigations"
 import { CheckCheck, ChevronDown, Heart, LogOut, MoveRight, Settings, Ticket } from "lucide-react"
+import { signOut, useSession } from "next-auth/react"
 import { useTranslations } from "next-intl"
 import Image from "next/image"
+import { Skeleton } from "./ui/skeleton"
 
 const menuOptions = [
     {
@@ -52,6 +54,10 @@ export const UserMenu = () => {
 
     const pathname = usePathname();
     const t = useTranslations('UserMenu');
+    const { data: session, status } = useSession()
+
+    console.log(session);
+
 
     return (
         <DropdownMenu>
@@ -60,8 +66,15 @@ export const UserMenu = () => {
                 <Button variant={"ghost"}>
                     <div className="flex justify-between w-45 items-center h-full cursor-pointer">
                         <div className="flex justify-center items-center">
-                            <Image src="/user-photo.jpg" width={606} height={680} alt="user-photo" className="h-6 w-6 object-cover shadow-[0_2px_6px_rgba(0,0,0,0.5)] text-foreground rounded-full" />
-                            <div className="ml-2 text-[16px] font-normal">{t("Username")}</div>
+                            {
+                                (status == "authenticated" || status == "loading") && session?.user?.image ? (
+                                    <Image width={96} height={96} src={session?.user?.image} alt="User Avatar" loading="eager" className="h-6.5 w-6.5 object-cover shadow-[0_2px_6px_rgba(0,0,0,0.5)] text-foreground rounded-full" />)
+                                    : (<Skeleton className="h-6.5 w-6.5 rounded-full" />)
+                            }
+                            {
+                                (status == "authenticated" || status == "loading") && session?.user?.name ? (<div className="ml-2 text-[16px] font-normal">{session?.user?.name}</div>)
+                                    : (<Skeleton className="ml-2 w-28 h-3" />)
+                            }
                         </div>
                         <ChevronDown />
                     </div>
@@ -111,7 +124,7 @@ export const UserMenu = () => {
 
                 {/* Logout */}
                 <DropdownMenuGroup>
-                    <DropdownMenuItem className="justify-between" /* //TODO: onClick={() => onLogout()} */>
+                    <DropdownMenuItem className="justify-between" onClick={() => signOut()}>
                         <span>{t('Logout')}</span>
                         <LogOut />
                     </DropdownMenuItem>
@@ -119,6 +132,6 @@ export const UserMenu = () => {
 
             </DropdownMenuContent>
 
-        </DropdownMenu>
+        </DropdownMenu >
     )
 }
