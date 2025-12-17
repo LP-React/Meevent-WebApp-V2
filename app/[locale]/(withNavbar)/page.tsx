20667ac96296bb09517477b0519bc77ff3ffc9ea
Laysson Polo <109695735+LP-React@users.noreply.github.com>
2025-12-17 16:41:09 -0500

    feat/next-cloudinary setup (#24)
    * fix: pnpm-lock
    
    * refactor: main -page

--- app/[locale]/(withNavbar)/page.tsx
+++ app/[locale]/(withNavbar)/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
 
-          hola
         </div>
       </main>
     </div>
